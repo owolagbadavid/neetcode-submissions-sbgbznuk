5a4ab469a9f706198ac97f42617e2e47bd2d7f32
DAVID <owolagbadavid@gmail.com>
2026-05-05 07:01:53 +0100

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.go
@@ -0,0 +1,29 @@
+func merge(intervals [][]int) [][]int {
+    res := [][]int{}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] < intervals[j][1]
+		}
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	last := intervals[0]
+	for i := 1; i <= len(intervals); i++ {
+		fmt.Println("%v %v", last, i)
+
+		if i < len(intervals) && last[1] >= intervals[i][0] {
+			last[1] = max(intervals[i][1], last[1])
+		} else {
+			res = append(res, last)
+			if i < len(intervals) {
+				last = intervals[i]
+			}
+		}
+	}
+
+	fmt.Println("%v", intervals)
+	fmt.Println("%v", res)
+
+	return res
+}
